13c5826948b943337ebd61b110b215c4df90d515
nileshkardile <61143934+nileshkardile@users.noreply.github.com>
2022-08-02 12:49:15 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,5 +1,11 @@
 #!/usr/bin/env groovy
-
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.*
+import java.net.URL
 def info(msg) {
   echo "Hello, ${msg}."
 }
